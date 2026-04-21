57407f65a89eab4123f8e3c6949f05531c5c7e9e
Luigi Cavalli <luigi_cavalli@outlook.com>
2026-04-22 01:07:29 +0200

    Created test files in frontend and fixed CI script

--- frontend/src/feature/pages/index/index.component.ts
+++ frontend/src/feature/pages/index/index.component.ts
@@ -5,27 +5,10 @@
  */
 import { Component } from "@angular/core";
 
-/**
- * ----------
- * COMPONENTS
- * ----------
- */
-import { ButtonComponent } from "../../components/shared/button/button.component";
-import { InputComponent } from "../../components/shared/input/input.component";
-import { SelectComponent } from "../../components/shared/select/select.component";
-import { DatepickerComponent } from "../../components/shared/datepicker/datepicker.component";
-
 
 @Component({
   selector:    'app-index',
-  imports: [ButtonComponent, InputComponent, SelectComponent, DatepickerComponent],
   styleUrl:    './index.component.scss',
   templateUrl: './index.component.html'
 })
-export class IndexComponent {
-  options = [
-    { label: 'Designer',   value: 'designer' },
-    { label: 'Developer',  value: 'developer' },
-    { label: 'Manager',    value: 'manager', disabled: true },
-  ];
-};
+export class IndexComponent {};
