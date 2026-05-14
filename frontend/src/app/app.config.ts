764a11d03e111a687fb5d67104441703c0034a05
Luigi Cavalli <luigi_cavalli@outlook.com>
2026-05-14 21:14:51 +0200

    Initialized internationalization (i18n)

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -8,6 +8,8 @@ import {
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideRouter, Router }               from '@angular/router';
 import * as Sentry                             from '@sentry/angular';
+import { provideTranslateService }             from '@ngx-translate/core';
+import { provideTranslateHttpLoader }          from '@ngx-translate/http-loader';
 
 import { AuthService }    from '../services/auth.service';
 import { authInterceptor } from './interceptors/auth.interceptor';
@@ -21,6 +23,8 @@ export const appConfig: ApplicationConfig = {
     provideHttpClient(
       withInterceptors([authInterceptor]),
     ),
+    provideTranslateService({ lang: 'it' }),
+    provideTranslateHttpLoader({ prefix: '/i18n/', suffix: '.json' }),
     { provide: ErrorHandler, useValue: Sentry.createErrorHandler() },
     { provide: Sentry.TraceService, deps: [Router] },
     provideAppInitializer(() => inject(AuthService).init()),
